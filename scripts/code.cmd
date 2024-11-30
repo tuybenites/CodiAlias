7563e20aa2d8968b6ca1bba6a871a3563dc25cb5
Arthur Benites <arthurbenites@protonmail.com>
2024-11-29 23:58:04 -0300

    feat: add scripts to be copied
    Added the scripts that will be copied to the selected folders.

---
+++ scripts/code.cmd
@@ -0,0 +1,8 @@
+@echo off
+"%~dp0Codium.cmd" %*
+
+REM ===========================================
+REM Script created by Arthur Benites da Silva
+REM Repository: github.com/tuybenites/CodiAlias  
+REM Please review the license terms before redistributing this script.
+REM ===========================================
